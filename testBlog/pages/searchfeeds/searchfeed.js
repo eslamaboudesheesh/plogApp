7a9277eae0528fefad0e4a83e71e680dbec5b8d5
EslamAboudesheesh <t-emhussein@EFG-HERMES.com>
2020-02-09 13:33:08 +0200

    add login screen

--- testBlog/pages/searchfeeds/searchfeed.js
+++ testBlog/pages/searchfeeds/searchfeed.js
@@ -12,3 +12,8 @@ $("#inpt_search").on('blur', function () {
 	if($(this).val().length == 0)
 		$(this).parent('label').removeClass('active');
 });
+
+
+$('.button__openmodel').click(function(){
+   $('#open__model').toggle();
+});
