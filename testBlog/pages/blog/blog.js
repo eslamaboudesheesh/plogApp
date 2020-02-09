0cf04f1bc4b7b3c733b7f1390db2d676d6912639
EslamAboudesheesh <t-emhussein@EFG-HERMES.com>
2020-02-09 15:21:14 +0200

    add bloog page

---
+++ testBlog/pages/blog/blog.js
@@ -0,0 +1,19 @@
+function myFunction(num) {
+
+    var popup = document.getElementById("myPopup"+num);
+    popup.classList.toggle("show");
+  }
+
+  $("#inpt_search").on('focus', function () {
+	$(this).parent('label').addClass('active');
+});
+
+$("#inpt_search").on('blur', function () {
+	if($(this).val().length == 0)
+		$(this).parent('label').removeClass('active');
+});
+
+
+$('.button__openmodel').click(function(){
+   $('#open__model').toggle();
+});
